b7c0b37572ea8fbcec6212e7b45108bd6a5fc679
PyaePhyoKyaw12 <pyaephyokyaw121318@gmail.com>
2024-10-07 11:54:20 +0630

    color

--- login.php
+++ login.php
@@ -21,10 +21,10 @@ session_start();
         <meta name="description" content="" />
         <meta name="author" content="" />
         <title>Login - SB Admin</title>
-        <link href="dist/css/styles.css" rel="stylesheet" />
+        <link href="css/styles.css" rel="stylesheet" />
         <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
     </head>
-    <body class="bg-primary">
+    <body class="bg-info">
         <div id="layoutAuthentication">
             <div id="layoutAuthentication_content">
                 <main>
@@ -36,9 +36,13 @@ session_start();
                                     <div class="card-body">
                                         <form method="post">
                                             <div class="form-floating mb-3">
+                                            
+                                            <i class="fas fa-user" ></i>
                                                 <input class="form-control" id="inputEmail" name="email" type="email" placeholder="name@example.com" />
-                                                <label for="inputEmail">Email address</label>
+                                                <label for="inputEmail">Enter Email or UserName</label>
                                             </div>
+
+                    
                                             <div class="form-floating mb-3">
                                                 <input class="form-control" id="inputPassword" name="password" type="password" placeholder="Password" />
                                                 <label for="inputPassword">Password</label>
@@ -62,22 +66,13 @@ session_start();
                     </div>
                 </main>
             </div>
-            <div id="layoutAuthentication_footer">
-                <footer class="py-4 bg-light mt-auto">
-                    <div class="container-fluid px-4">
-                        <div class="d-flex align-items-center justify-content-between small">
-                            <div class="text-muted">Copyright &copy; Your Website 2023</div>
-                            <div>
-                                <a href="#">Privacy Policy</a>
-                                &middot;
-                                <a href="#">Terms &amp; Conditions</a>
-                            </div>
-                        </div>
-                    </div>
-                </footer>
-            </div>
+            
         </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
         <script src="dist/js/scripts.js"></script>
     </body>
+
+    <style>
+        
+    </style>
 </html>
